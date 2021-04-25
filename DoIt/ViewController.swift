8a1e0197f35588692c7947ba5937c49635940fbc
Alexx <>
2021-04-24 23:41:36 -0400

    first commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Alejandro Diaz Sotolongo on 4/24/21.
+//  Copyright © 2021 aaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
